b3a61d94385d19d72ccade900dc8142094886608
chinnasivakrishna <139040548+chinnasivakrishna@users.noreply.github.com>
2024-07-29 16:55:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ function ignoreFavicon(req, res, next) {
 }
 app.use(ignoreFavicon);
 // Schedule the cron job to run at the specified time (e.g., 10:15 AM every day)
-cron.schedule('54 16 * * *', () => {
+cron.schedule('57 16 * * *', () => {
   console.log("hii")
   getDataFromDB();
   console.log('Scheduled task ran at the specified time');
