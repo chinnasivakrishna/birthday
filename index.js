36288e285bc45cc835df9ef8dfe9fb2db911f9b5
chinnasivakrishna <139040548+chinnasivakrishna@users.noreply.github.com>
2025-08-30 00:10:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -53,10 +53,9 @@ const getDataFromDB = async () => {
 
     for (let i = 0; i < data.length; i++) {
       const date1 = data[i].DOB;
-      console.log(date1)
       const month1 = date1.getMonth() + 1;
       const day1 = date1.getDate();
-      console.log(date1)
+      console.log(day,day1)
       if (day == day1 && month == month1) {
         console.log(`It's ${data[i].EmpName}, ${data[i]._id},${data[i].DOB}'s birthday today!`);
         // Send email asynchronously
